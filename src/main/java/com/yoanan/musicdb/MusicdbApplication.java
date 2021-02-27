a2047070b60a66aa73a5e9155473fa77b6f91e67
yoananaydenova <y.naydenova@gmail.com>
2021-02-27 20:44:58 +0200

    Initial commit

---
+++ src/main/java/com/yoanan/musicdb/MusicdbApplication.java
@@ -0,0 +1,13 @@
+package com.yoanan.musicdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicdbApplication.class, args);
+    }
+
+}
