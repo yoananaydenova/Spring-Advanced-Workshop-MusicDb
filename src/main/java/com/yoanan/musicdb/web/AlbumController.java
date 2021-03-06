5fbb6121523ebb287f2935e274d41f1298717484
yoananaydenova <y.naydenova@gmail.com>
2021-03-06 23:17:30 +0200

    Create AlbumController - part 1.

---
+++ src/main/java/com/yoanan/musicdb/web/AlbumController.java
@@ -0,0 +1,15 @@
+package com.yoanan.musicdb.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/albums")
+public class AlbumController {
+
+    @GetMapping("/add")
+    public String addAlbum(){
+        return "add-album";
+    }
+}
