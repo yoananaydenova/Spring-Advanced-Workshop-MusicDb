b1ba2e9fc5c0c9492a51ca02cf71fd86305b01dd
yoananaydenova <y.naydenova@gmail.com>
2021-03-10 18:33:43 +0200

    Error handling - 404, 500, generic errors.

---
+++ src/main/java/com/yoanan/musicdb/web/MusicDBErrorController.java
@@ -0,0 +1,35 @@
+package com.yoanan.musicdb.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MusicDBErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest httpServletRequest){
+
+        Object status = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode== HttpStatus.NOT_FOUND.value()){
+                return "error-404";
+            }else if(statusCode== HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
